926c870f6c8699bef4a81b69ea7cde3790cb5242
nuclearmistake <nuclearmistake@gmail.com>
2016-06-01 02:04:22 -0400

    Revert "Only preopt user builds of angler"
    This reverts commit 1378c12def4fad1004ce808dad7c516a4d25aae6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
